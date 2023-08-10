ab4ea935e16e5b642edcf63d5cb413a4f47fbc1b
Odinaka nnamani <nnamani.odinakarobert@gmail.com>
2023-08-10 14:51:43 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints a required text to the standard error
+ *
+ * description: A simple c script to complie and run a message
+ *
+ * Return: 1 if successful
+ */
+int main(void)
+{
+	printf("and that piece of art is useful\" -Dora Korpar, 2015-10-19\n");
+	printf("2, message, sizeof(message) - 1\n");
+	return (1);
+}
