ec3cb4199d72480b0458fdb75485caa7a9957608
Odinaka nnamani <nnamani.odinakarobert@gmail.com>
2023-08-10 12:28:38 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
-#include <stdio.h>
-int main (void) {
-	puts("Programming is like building a multilingual puzzle,");
-	return 0;
-}	
+#include<stdio.h>
+/**
+ * main - The main function for execution
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
