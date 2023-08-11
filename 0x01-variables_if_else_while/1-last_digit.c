b1eaad2b306a771d4092fee9880fbe13f195bea8
Odinaka nnamani <nnamani.odinakarobert@gmail.com>
2023-08-11 07:26:29 -0700

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,12 +14,12 @@ int main(void)
 	printf("Last digit of %d is", n);
 	if (n > 5)
 	{
-		printf("and is greater than5\n");
+		printf("%d and is greater than 5\n", n);
 	} else if (n == 0)
-	{	printf("and is 0\n");
+	{	printf("%d and is 0\n", n);
 	}
 	else
-	{	printf("and is less than 6 and not 0\n");
+	{	printf("%d is less than 6 and not 0\n", n);
 	}
 	return (0);
 }
