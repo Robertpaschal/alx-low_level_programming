00ba34acf73d3f0db429d5861f05da7c25d29e3b
Odinaka nnamani <nnamani.odinakarobert@gmail.com>
2023-08-12 03:33:09 -0700

    print

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,10 +8,10 @@ int main(void)
 {
 	int d, p, q;
 
-	for (d = '0'; d < '9'; d++)
+	for (d = '0'; d < '7'; d++)
 	{
 
-	for (p = d + 1; p <= '9'; p++)
+	for (p = d + 1; p <= '8'; p++)
 	{
 
 	for (q = p + 1; q <= '9'; q++)
@@ -21,7 +21,7 @@ int main(void)
 	putchar(d);
 	putchar(p);
 	putchar(q);
-	if (d == 7 && p == 8)
+	if (d == 7 && p == 8 && q == 9)
 		continue;
 	putchar(',');
 	putchar(' ');
