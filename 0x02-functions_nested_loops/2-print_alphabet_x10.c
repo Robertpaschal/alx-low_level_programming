1decd46d2cb4379d20a52a8d228a61cac6c67542
Odinaka nnamani <nnamani.odinakarobert@gmail.com>
2023-08-15 13:27:29 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints alphabets ten times
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char j = 0, i;
+
+	for (j = 0; j <= 10; i++)
+	{
+		for (i = 97; i <= 122; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+}
