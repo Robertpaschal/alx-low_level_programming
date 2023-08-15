83363c40a289e7f93d2f1f6e4ad9e9a508de304f
Odinaka nnamani <nnamani.odinakarobert@gmail.com>
2023-08-15 09:17:14 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description; prints a program and a new line
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
