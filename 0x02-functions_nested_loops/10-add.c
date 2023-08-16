dd26c9b5422eaa697d2809a5e98fcffa81a04f20
Odinaka nnamani <nnamani.odinakarobert@gmail.com>
2023-08-15 19:50:51 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - Adds two value
+ * @n3: The first number computed
+ * @n4: The second number computed
+ * Return: Intger value
+ */
+int add(int n3, int n4)
+{
+	return (n3 + n4);
+}
