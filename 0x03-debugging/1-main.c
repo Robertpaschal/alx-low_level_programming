4ee65194afae1483b4968d0ab44ca41695bc22d2
Odinaka nnamani <nnamani.odinakarobert@gmail.com>
2023-08-17 02:55:25 -0700

    main

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -2,7 +2,6 @@
 
 /**
 * main - causes an infinite loop
-* @\\o/\n'': the comment that should be included in an output
 * Return: 0
 */
 
@@ -11,15 +10,14 @@ int main(void)
 	int i;
 
 	printf("Infinite loop incoming :(\n");
-/**
-* comment out for infinite loop
-*	i = 0;
-*
-*	while (i < 10)
-*	{
-*		putchar(i);
-*	}
-*/
+
+	i = 0;
+
+	/*while (i < 10)*/
+	/*{*/
+	/*	putchar(i);*/
+	/*}*/
+
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
