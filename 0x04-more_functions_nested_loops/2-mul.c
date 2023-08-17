3e2476805d602278fd546a877164a10e460371d2
Odinaka nnamani <nnamani.odinakarobert@gmail.com>
2023-08-17 03:24:34 -0700

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - multiples two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+	int i = a * b;
+
+	return (i);
+}
