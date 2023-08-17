4e19163e09a2b8ea5c0ba8a9c4c2c3a3907b07ca
Odinaka nnamani <nnamani.odinakarobert@gmail.com>
2023-08-17 13:25:08 -0700

    line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_line - draws a straight line
+ * @n: number of times the character should be printed
+ * Return: 0 (success)
+ */
+
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (int i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
