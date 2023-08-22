08cec3550b4fb298eb41d9d2c3b94ff00d12394f
Odinaka nnamani <nnamani.odinakarobert@gmail.com>
2023-08-22 08:35:40 -0700

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -4,36 +4,34 @@
 /**
  * _atoi - convert a string to an integer
  * @s: the string to be converted
- * Return: Always 0
+ * Return: The integer  value
  */
 int _atoi(char *s)
 {
-	int sign = 1, result = 0;
+	int sign = 1;
+	int num = 0;
+	int i = 0;
 
-	if (*s == '-')
+	while ((s[i] < '0' || s[i] > '9') && s[i] != '\0')
 	{
-		sign = -1;
-		s++;
+		if (s[i] == '-')
+			sign *= -1;
+		i++;
 	}
-	else if (*s == '+')
+
+	while (s[i] >= '0' && s[i] <= '9')
 	{
-		s++;
-	}
-
-
-
-	while (*s != '\0')
-	{
-		if (*s >= '0' && *s <= '9')
+		if (num > (INT_MAX - (s[i] - '0')) / 10)
 		{
-			result = result * 10 + (*s - '0');
+			if (sign == 1)
+				return (INT_MAX);
+			else
+				return (INT_MIN);
 		}
-		else
-		{
-			break;
-		}
-		s++;
+
+		num = num * 10 - (s[i] - '0');
+		i++;
 	}
 
-	return (result * sign);
+	return (num * sign);
 }
