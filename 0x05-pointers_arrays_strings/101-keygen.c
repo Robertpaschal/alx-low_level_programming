4131573d2bbf2ff738915491b5d4af64e1c45082
Odinaka nnamani <nnamani.odinakarobert@gmail.com>
2023-08-22 13:13:59 -0700

    puts

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,31 +2,32 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define PASSWORD_LENGTH 12
-
 /**
  * main - generates random password
  * Return: Always 0
  */
 
-int main()
+int main(void)
 {
-	char password[PASSWORD_LENGTH + 1];
-	int i;
+	int pass[100];
+	int i, sum, n;
 
-	char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-	int charsetSize = sizeof(charset) - 1;
+	sum = 0;
 
 	srand(time(NULL));
 
-	for (i = 0; i < PASSWORD_LENGTH; i++)
+	for (i = 0; i < 100; i++)
 	{
-		int index = rand() % charsetSize;
-		password[i] = charset[index];
+		pass[i] = rand() % 78;
+		sum += (pass[i] + '0');
+		putchar(pass[i] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+			n = 2772 - sum - '0';
+			sum += n;
+			putchar(n + '0');
+			break;
+		}
 	}
-	password[PASSWORD_LENGTH] = '\0';
-
-	printf("Generated Password: %s\n", password);
-
 	return (0);
 }
