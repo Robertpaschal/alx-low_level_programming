26a974faec174efec36fa89b96ffd600d86d67d6
Odinaka nnamani <nnamani.odinakarobert@gmail.com>
2023-08-24 02:42:21 -0700

    leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -11,11 +11,10 @@ char *leet(char *n)
 	int i, j;
 	char s1[] = "aAeEoOtTIL";
 	char s2[] = "4433007711";
-	int len = strlen(s1);
 
 	for (i = 0; n[i] != '\0'; i++)
 	{
-		for (j = 0; j < len; j++)
+		for (j = 0; j < 10; j++)
 		{
 			if (n[i] == s1[j])
 			{
