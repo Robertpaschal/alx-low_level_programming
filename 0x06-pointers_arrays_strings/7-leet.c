648609df89270aa7b38b981979609b82c1bde506
Odinaka nnamani <nnamani.odinakarobert@gmail.com>
2023-08-24 11:55:40 -0700

    leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 /**
  * leet - encode a string
  * @n: input value
@@ -10,10 +11,11 @@ char *leet(char *n)
 	int a, b;
 	char s1[] = "aAeEoOtTIL";
 	char s2[] = "4433007711";
+	int len = strlen(s1);
 
 	for (a = 0; n[a] != '\0'; a++)
 	{
-		for (b = 0; b < 10; b++)
+		for (b = 0; b < len; b++)
 		{
 			if (n[a] == s1[b])
 			{
