0434cc0901092c3d6924c10efb621cf50a38b77e
Odinaka nnamani <Robertpaschal>
2023-08-25 05:55:47 -0700

    buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -18,11 +18,10 @@ void print_buffer(char *b, int size)
 		printf("\n");
 		return;
 	}
-
 	while (o < size)
 	{
 		j = size - o < 10 ? size - o : 10;
-		printf("%08x: ", o);
+		printf("%08x:  ", o);
 		for (i = 0; i < 10; i++)
 		{
 			if (i < j)
