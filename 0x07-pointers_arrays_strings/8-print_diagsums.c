e7194b6b427c9e5d3335a74b0ecf62b94913e3dd
Odinaka nnamani <Robertpaschal>
2023-08-28 09:24:23 -0700

    diagsums

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,13 +9,18 @@
 
 void print_diagsums(int *a, int size)
 {
-	int sum1 = 0;
-	int sum2 = 0;
+	int sum1;
+	int sum2;
 	int i;
+	sum1 = 0;
+	sum2 = 0;
 
 	for (i = 0; i < size; i++)
 	{
 		sum1 += a[i * size + i];
+	}
+	for (i = size - 1; i >= 0; i--)
+	{
 		sum2 += a[i * size + (size - 1 - i)];
 	}
 
