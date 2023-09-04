448a47343c1fe28b03ca14864b35fc3b2f8f85ed
Odinaka nnamani <Robertpaschal>
2023-09-04 08:34:39 -0700

    free

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * free_grind - frees a 2 dimensional grid
+ * @*grid: grid of array
+ * @height: height
+ * Return: Nothing
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	if (grid == NULL || height <= 0)
+	{
+		return;
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+
+	free(grid);
+}
