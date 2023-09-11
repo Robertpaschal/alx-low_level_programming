f6006ff0b835096ad0cfa874b767615610844894
Odinaka nnamani <Robertpaschal>
2023-09-11 10:41:51 -0700

    main

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints source file for compilation
+ * @void: input
+ * Return: 0(success)
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
