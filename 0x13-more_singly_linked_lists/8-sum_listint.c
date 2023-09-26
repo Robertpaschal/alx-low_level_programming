9448b04e6a8c41ae91ad16fe9d5da0c3ebb1895f
Odinaka nnamani <Robertpaschal>
2023-09-26 02:41:43 -0700

    sum

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 #include "lists.h"
 /**
- * sum_listsint - returns the sum of all the data (n) of a listint_t linked list
+ * sum_listint - returns the sum of all the data (n) of a
+ * listint_t linked list
  * @head: first node in the linked list
  * Return: resulting sum
  */
-int sun_listint(listint_t *head)
+int sum_listint(listint_t *head)
 {
 	int sum = 0;
+	listint_t *temp = head;
 
 	while (head != NULL)
 	{
-		sum += head->n;
-		head = head->next;
+		sum += temp->n;
+		temp = temp->next;
 	}
 
 	return (sum);
