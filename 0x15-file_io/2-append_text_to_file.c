3366e34ad8276bc325a7ffa543b1b7f047ca2eb3
Odinaka nnamani <Robertpaschal>
2023-10-02 18:01:47 -0700

    append

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * append_text_to_file - appends text to file
+ * @filename: name of the file
+ * @text_content: content of the text
+ * Return: 1 on success and -1 on failure
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t bytesWritten;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content == NULL)
+		return (1);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+
+	bytesWritten = write(fd, text_content, strlen(text_content));
+
+	if (bytesWritten == -1)
+	{
+		perror("write");
+		close(fd);
+		return (-1);
+	}
+
+	close(fd);
+	return (1);
+}
