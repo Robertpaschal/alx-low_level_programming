c5f390f8c54ac7e0fda9a77a3a13ec7f721b954d
Odinaka <nnamani.odinakarobert@gmail.com>
2024-01-24 09:21:17 -0800

    perimeter

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+def island_perimeter(grid):
+    """
+    Calculate the perimeter of the islnad described in the grid.
+
+    Parameters:
+    - grid (list of list of intergers): Grid representing the island.
+
+    Returns: 
+    - int: Perimeter of the island
+    """
+
+    if not grid or not grid[0]:
+        return 0
+
+    rows, cols = len(grid), len(grid[0])
+    perimeter = 0
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                perimeter += 4
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j -1] == 1:
+                    perimeter -= 2
+
+    return perimeter
